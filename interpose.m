640faf5dc9de17c4ee249e15ada6737d68d974c9
nex403 <nex4033@gmail.com>
2026-06-04 04:37:36 +0300

    Update interpose.m

--- interpose.m
+++ interpose.m
@@ -1,18 +1,27 @@
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
-#import <mach-o/dyld-interposing.h>
+#import <mach-o/dyld.h>
 
-// 1. تعيين إحداثيات الموقع المزيف الذي تريده (الرياض كمثال)
+// 1. هيكل البيانات الرسمي الخاص بـ DYLD Interposing من آبل
+typedef struct dyld_interpose_tuple {
+    const void* replacement;
+    const void* original;
+} dyld_interpose_tuple;
+
+// 2. تعيين الإحداثيات المزيفة (الرياض)
 static double const kFakeLatitude = 24.7136;
 static double const kFakeLongitude = 46.6753;
 
-// 2. الدالة البديلة الخاصة بنا
-CLLocationCoordinate2D custom_coordinate(CLLocation *self, SEL _cmd) {
+// 3. الدالة البديلة التي ستستبدل دالة النظام الأصلية
+CLLocationCoordinate2D my_coordinate(CLLocation *self, SEL _cmd) {
     CLLocationCoordinate2D fakeCoord;
     fakeCoord.latitude = kFakeLatitude;
     fakeCoord.longitude = kFakeLongitude;
     return fakeCoord;
 }
 
-// 3. جدول الـ Interposing لتبديل الدالة الرسمية بالدالة البديلة
-DYLD_INTERPOSE(custom_coordinate, -[CLLocation coordinate]);
+// 4. مصفوفة الـ Interpose لحقن الدالة في قطاع الـ __DATA داخل الـ Binary
+__attribute__((used)) static const dyld_interpose_tuple interposing_map[] \
+__attribute__((section("__DATA,__interpose"))) = {
+    { (const void*)(my_coordinate), (const void*)_sel_registerName("coordinate") }
+};
